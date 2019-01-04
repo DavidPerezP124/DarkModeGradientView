795acf9c9cc75208a74c4a7f85481d8b68f95f10
David Perez <D.Perez@davids-macbook-air.local>
2019-01-04 15:42:12 -0600

    Initial Commit

---
+++ DarkModeGradientView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DarkModeGradientView
+//
+//  Created by David Perez on 1/4/19.
+//  Copyright © 2019 David Perez P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
